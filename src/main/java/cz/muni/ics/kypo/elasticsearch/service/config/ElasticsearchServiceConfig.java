af259d0d5ba258784d51a39d206f6e6b037f4180
Dominik Pilár <xpilar3@fi.muni.cz>
2020-06-25 17:52:53 +0200

    Added security to service

--- src/main/java/cz/muni/ics/kypo/elasticsearch/service/config/ElasticsearchServiceConfig.java
+++ src/main/java/cz/muni/ics/kypo/elasticsearch/service/config/ElasticsearchServiceConfig.java
@@ -1,11 +1,12 @@
 package cz.muni.ics.kypo.elasticsearch.service.config;
 
+import cz.muni.ics.kypo.commons.security.config.ResourceServerSecurityConfig;
 import cz.muni.ics.kypo.elasticsearch.data.config.ElasticsearchDataConfig;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import({ElasticsearchDataConfig.class})
+@Import({ElasticsearchDataConfig.class, ResourceServerSecurityConfig.class})
 public class ElasticsearchServiceConfig {
 
 }
